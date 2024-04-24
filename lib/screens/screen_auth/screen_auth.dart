f77d40be4ec75bcdf99663f376fbd26b1f2b6815
sanin <saninthottungalhere@gmail.con>
2024-04-24 23:45:08 +0530

    screen Auth completed

--- lib/screens/screen_auth/screen_auth.dart
+++ lib/screens/screen_auth/screen_auth.dart
@@ -10,77 +10,88 @@ class ScreenAuth extends StatelessWidget {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(width: double.infinity, height: 55),
-          Container(
-            height: height * 0.4,
-            width: width * 0.7,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: Colors.blue,
-                width: 5,
-                strokeAlign: BorderSide.strokeAlignOutside,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 35, horizontal: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const SizedBox(width: double.infinity),
+            Container(
+              height: height * 0.4,
+              width: width * 0.7,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.blue,
+                  width: 5,
+                  strokeAlign: BorderSide.strokeAlignOutside,
+                ),
+                image: const DecorationImage(
+                  fit: BoxFit.fitHeight,
+                  image: AssetImage('assets/images/auth.jpg'),
+                ),
               ),
-              image: DecorationImage(
-                fit: BoxFit.fitHeight,
-                image: AssetImage('assets/images/auth.jpg'),
+            ),
+            const Text(
+              "Hello",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
+            const Text(
+              "Welcome to fairflights\ni'll be your mentor travel partner",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 16,
+              ),
             ),
-          ),
-          Text(
-            "Welcome to fairflights\nwhere you can book flights tickets",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.black54,
-              fontSize: 16,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: SizedBox(
+                width: 250,
+                height: 45,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      foregroundColor:
+                          const MaterialStatePropertyAll(Colors.white),
+                      backgroundColor:
+                          MaterialStatePropertyAll(AppColor.customBlue)),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/login');
+                  },
+                  child: const Text("Login"),
+                ),
+              ),
             ),
-          ),
-          SizedBox(height: 25),
-          SizedBox(
-            width: 250,
-            height: 45,
-            child: ElevatedButton(
-              style: ButtonStyle(
-                  foregroundColor: const MaterialStatePropertyAll(Colors.white),
-                  backgroundColor:
-                      MaterialStatePropertyAll(AppColor.customBlue)),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/login');
-              },
-              child: Text("Login"),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: SizedBox(
+                width: 250,
+                height: 45,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Sign Up"),
+                ),
+              ),
             ),
-          ),
-          SizedBox(height: 10),
-          SizedBox(
-            width: 250,
-            height: 45,
-            child: ElevatedButton(onPressed: () {}, child: Text("Sign Up")),
-          ),
-          SizedBox(height: 35),
-          Text(
-            "or",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.black54,
-              fontSize: 16,
+            const SizedBox(height: 10),
+            const Text(
+              "or sign in using",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 16,
+              ),
             ),
-          ),
-          SizedBox(height: 10),
-          Image.asset(
-            'assets/images/google.png',
-            height: 50,
-            width: 50,
-          ),
-        ],
+            Image.asset(
+              'assets/images/google.png',
+              height: 50,
+              width: 50,
+            ),
+          ],
+        ),
       ),
     );
   }

7f48f8b32497136e11eb907dfb9de9784938bdbb
sanin <saninthottungalhere@gmail.con>
2024-04-26 15:20:00 +0530

    rewrote auth functions, yet to check

--- lib/services/auth/auth_functions.dart
+++ lib/services/auth/auth_functions.dart
@@ -1,4 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flight_booking/services/connectivty/check_connectivty.dart';
+import 'package:flight_booking/services/exception/auth_exceptions.dart';
+import 'package:flight_booking/services/exception/network_exceptions.dart';
 
 class AuthFunctions {
   final FirebaseAuth _autInstance;
@@ -7,37 +10,130 @@ class AuthFunctions {
 
   //signin
   Future<void> signIn({required String email, required String password}) async {
-    await _autInstance.signInWithEmailAndPassword(
-        email: email, password: password);
+    final isConnectionAvailable =
+        await CheckNetConnectivity().checknetConnectivity();
+    if (!isConnectionAvailable) {
+      throw Network404Exception();
+    }
+    try {
+      await _autInstance.signInWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'invalid-email':
+          throw InvalidEmailException();
+        case 'user-disabled':
+          throw UserDisabledException();
+        case 'user-not-found':
+          throw UserNotFoundException();
+        case 'wrong-password':
+          throw WrongPasswordException();
+        default:
+          throw GenericException();
+      }
+    } catch (_) {
+      throw GenericException();
+    }
   }
 
   //signup
   Future<void> signUp({required String email, required String password}) async {
-    await _autInstance.createUserWithEmailAndPassword(
-        email: email, password: password);
+    final isConnectionAvailable =
+        await CheckNetConnectivity().checknetConnectivity();
+    if (!isConnectionAvailable) {
+      throw Network404Exception();
+    }
+    try {
+      await _autInstance.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'email-already-in-use':
+          throw EmailAlreadyInUseException();
+        case 'invalid-email':
+          throw InvalidEmailException();
+        case 'operation-not-allowed':
+          throw OperationNotAllowedException();
+        case 'weak-password':
+          throw WeakPasswordException();
+        default:
+          throw GenericException();
+      }
+    } catch (_) {
+      throw GenericException();
+    }
   }
 
   //logout
   Future<void> signOut() async {
-    await _autInstance.signOut();
+    final isConnectionAvailable =
+        await CheckNetConnectivity().checknetConnectivity();
+    if (!isConnectionAvailable) {
+      throw Network404Exception();
+    }
+    try {
+      await _autInstance.signOut();
+    } catch (_) {
+      throw GenericException();
+    }
   }
 
   //resetPassword
   Future<void> resetPassword({required String email}) async {
-    await _autInstance.sendPasswordResetEmail(email: email);
+    final isConnectionAvailable =
+        await CheckNetConnectivity().checknetConnectivity();
+    if (!isConnectionAvailable) {
+      throw Network404Exception();
+    }
+    try {
+      await _autInstance.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case 'auth/invalid-email':
+          throw InvalidEmailException();
+        case 'auth/user-not-found':
+          throw UserNotFoundException();
+        default:
+          throw GenericException();
+      }
+    } catch (_) {
+      throw GenericException();
+    }
   }
 
   //mailVerification
   Future<void> mailVerification() async {
+    final isConnectionAvailable =
+        await CheckNetConnectivity().checknetConnectivity();
+    if (!isConnectionAvailable) {
+      throw Network404Exception();
+    }
     if (_autInstance.currentUser != null) {
-      await _autInstance.currentUser!.sendEmailVerification();
+      try {
+        await _autInstance.currentUser!.sendEmailVerification();
+      } catch (_) {
+        throw GenericException();
+      }
+    } else {
+      throw UserNotFoundException();
     }
   }
 
   //reload
   Future<void> reloadUser() async {
+    final isConnectionAvailable =
+        await CheckNetConnectivity().checknetConnectivity();
+    if (!isConnectionAvailable) {
+      throw Network404Exception();
+    }
     if (_autInstance.currentUser != null) {
-      await _autInstance.currentUser!.reload();
+      try {
+        await _autInstance.currentUser!.reload();
+      } catch (_) {
+        throw GenericException();
+      }
+    } else {
+      throw UserNotFoundException();
     }
   }
 }
---
+++ lib/services/exception/auth_exceptions.dart
@@ -0,0 +1,17 @@
+//signin exceptions
+
+class InvalidEmailException implements Exception {}
+
+class UserDisabledException implements Exception {}
+
+class UserNotFoundException implements Exception {}
+
+class WrongPasswordException implements Exception {}
+
+//signup exception
+
+class EmailAlreadyInUseException implements Exception {}
+
+class WeakPasswordException implements Exception {}
+
+class OperationNotAllowedException implements Exception {}
